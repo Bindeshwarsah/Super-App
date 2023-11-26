04dd766039c33c32e75b7f4c1338e5fa397f6305
bindeshwarsah <bindeshwark742@gmail.com>
2023-11-26 21:25:06 +0530

    added user profile card

--- super-app/src/App.js
+++ super-app/src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import RegistrationPage from './pages/RegistrationPage'
 import Category from './pages/Category'
+import ProfileInfo from "../src/components/Profile/ProfileInfo"
+import UserInfo from './components/Profile/UserInfo';
+import CategoryInfo from './components/Profile/CategoryInfo';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <>
-      <Routes>
+    {/* <CategoryInfo/> */}
+    {/* <UserInfo/> */}
+    {/* <ProfileInfo/> */}
+    <HomePage/>
+      {/* <Routes>
         <Route path="/" element={<RegistrationPage/>} />
         <Route path="category" element={<Category/>} />
-      </Routes>
+      </Routes> */}
     </>
   )
 }
---
+++ super-app/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./HomePage.module.css"
+import ProfileInfo from '../components/Profile/ProfileInfo'
+
+const HomePage = () => {
+  return (
+    <>
+        <ProfileInfo/>
+    </>
+  )
+}
+
+export default HomePage
