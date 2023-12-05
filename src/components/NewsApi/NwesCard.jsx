f2e70c689ad447f862b02522edfa96e255a012dc
bindeshwarsah <bindeshwark742@gmail.com>
2023-12-05 10:19:26 +0530

    added news api

--- src/components/NewsApi/NwesCard.jsx
+++ src/components/NewsApi/NwesCard.jsx
@@ -86,6 +86,102 @@
 
 
 
+// import React, { useEffect, useState } from 'react';
+// import styles from './NewsCard.module.css';
+// import Datetime from '../DateTime/Datetime';
+// import axios from 'axios';
+
+// const truncateText = (text, maxLength) => {
+//   if (text.length > maxLength) {
+//     return text.slice(0, maxLength) + '.......';
+//   }
+//   return text;
+// };
+
+// const NewsCard = () => {
+//   const [newsData, setNewsData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchRandomNews = async () => {
+//       try {
+//         // const apiKey = 'a0c975056bd9469eacf68a9429113a96'; // Replace with your News API key
+
+//         const apiKey = 'a1362e4a78110946d636806f4ced56eb';
+
+
+        
+
+
+//         // const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+
+//         const apiUrl = `https://gnews.io/api/v4/{endpoint}?apikey=${apiKey}`;
+
+
+
+//         const response = await axios.get(apiUrl);
+//         if (response.data && response.data.articles && response.data.articles.length > 0) {
+//           setNewsData(response.data.articles);
+//         } else {
+//           throw new Error('Invalid response format');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching news data:', error.message);
+//         setError('Error fetching news data. Please try again later.');
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchRandomNews();
+//   }, []);
+
+//   if (loading) {
+//     return <p className={styles.loadingMessage}>Loading news...</p>;
+//   }
+
+//   if (error) {
+//     return <p className={styles.errorMessage}>{error}</p>;
+//   }
+
+//   if (!newsData || newsData.length === 0) {
+//     return <p className={styles.errorMessage}>No news data available.</p>;
+//   }
+
+//   // Display a random news article
+//   const randomIndex = Math.floor(Math.random() * newsData.length);
+//   const randomArticle = newsData[randomIndex];
+
+//   return (
+//     <div className={styles.newsCard}>
+//       <div>
+//         {randomArticle && randomArticle.urlToImage && (
+//           <img src={randomArticle.urlToImage} alt="News" className={styles.newsImage} />
+//         )}
+//       </div>
+//       <div className={styles.heading}>
+//         <h2>{randomArticle && randomArticle.title}</h2>
+//         <div className={styles.date}>
+//           <Datetime backgroundColor="transparent" fontSize="16px" textColor="#FFF" width="15rem" />
+//         </div>
+//       </div>
+//       <div className={styles.about}>
+//         {randomArticle && randomArticle.description && (
+//           <p className={styles.description}>{truncateText(randomArticle.description, 130)}</p>
+//         )}
+//         {randomArticle && randomArticle.content && (
+//           <p className={styles.content}>{truncateText(randomArticle.content, 70)}</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NewsCard;
+
+
 import React, { useEffect, useState } from 'react';
 import styles from './NewsCard.module.css';
 import Datetime from '../DateTime/Datetime';
@@ -106,9 +202,10 @@ const NewsCard = () => {
   useEffect(() => {
     const fetchRandomNews = async () => {
       try {
-        const apiKey = 'a0c975056bd9469eacf68a9429113a96'; // Replace with your News API key
-        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+        const apiKey = 'a1362e4a78110946d636806f4ced56eb';
+        const apiUrl = `https://gnews.io/api/v4/top-headlines?country=us&token=${apiKey}`;
         const response = await axios.get(apiUrl);
+
         if (response.data && response.data.articles && response.data.articles.length > 0) {
           setNewsData(response.data.articles);
         } else {
@@ -137,15 +234,14 @@ const NewsCard = () => {
     return <p className={styles.errorMessage}>No news data available.</p>;
   }
 
-  // Display a random news article
-  const randomIndex = Math.floor(Math.random() * newsData.length);
-  const randomArticle = newsData[randomIndex];
+  // Display the first news article
+  const randomArticle = newsData[0];
 
   return (
     <div className={styles.newsCard}>
       <div>
-        {randomArticle && randomArticle.urlToImage && (
-          <img src={randomArticle.urlToImage} alt="News" className={styles.newsImage} />
+        {randomArticle && randomArticle.image && (
+          <img src={randomArticle.image} alt="News" className={styles.newsImage} />
         )}
       </div>
       <div className={styles.heading}>
